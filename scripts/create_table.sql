4ae1b8e0d1cafad85d9f668adefe066cfbf8fc66
HariHarran03 <130299085+HariHarran03@users.noreply.github.com>
2025-04-01 23:33:19 +0530

    create_table.sql

---
+++ scripts/create_table.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE TEMPORARY TABLE demo_customer_info (  
+    id INTEGER,  
+    first_name STRING,  
+    last_name STRING,  
+    company STRING,  
+    email STRING,  
+    workphone STRING,  
+    cellphone STRING,  
+    streetaddress STRING,  
+    city STRING,  
+    postalcode STRING  
+);
